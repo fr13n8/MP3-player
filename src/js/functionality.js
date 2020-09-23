d688d3cf3c1ead50efc1e79253337882205cd92a
fr13n8 <ovsepanrik@gmail.com>
2020-09-24 00:50:29 +0300

    Progress bar draggable

--- src/js/functionality.js
+++ src/js/functionality.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
         constructor( songs ) {
             this.songs = songs;
             this.index = 0;
+            this.reply = false;
+            this.onHover = false;
             this.showDetails();
         }
 
@@ -33,14 +35,17 @@ $(document).ready(function () {
                     // loop: true,
                     volume: 0.1,
                     html5: true,
-                    onend: function() {
-                        this.skip('next');
+                    onend: () => {
+                        this.reply ? this.play() : this.skip('next', true) ;
                     },
                     onseek: () => {
                         requestAnimationFrame(this.step.bind(this));
                     },
                     onplay: () => {
+                        var song = this.songs[this.index];
+                        let duration = song.metaData.format.duration;
                         requestAnimationFrame(this.step.bind(this));
+                        $('#progress-bar').attr('max', Math.round(duration));
                     }
                     // autoplay: false
                 });
@@ -53,8 +58,8 @@ $(document).ready(function () {
             var sound = this.songs[this.index].howl;
             var seek = sound.seek() || 0;
             $(".timer").html(MusicController.formatTime(Math.round(seek)));
-            $("#progress-bar").css('width', (((seek / sound.duration()) * 100) || 0) + '%');
-            if (sound.playing()) {
+            $("#progress-bar").val(Math.round(seek) || 0);
+            if (sound.playing() && !this.onHover) {
                 requestAnimationFrame(this.step.bind(this));
             }
         }
@@ -67,10 +72,15 @@ $(document).ready(function () {
             this.songs[this.index].howl.stop();
         }
 
-        skip(direction) {
+        setReply() {
+            this.reply = !this.reply;
+        }
+
+        skip(direction, play) {
             let index = this.index;
-            let isPlayed = this.songs[index].howl.playing();
-            this.songs[index].howl.stop();
+            let isPlayed = this.songs[index].howl ? this.songs[index].howl.playing() : false;
+            console.log(isPlayed)
+            if(isPlayed) this.stopSong();
             switch (direction) {
                 case 'next':
                     index += 1;
@@ -88,7 +98,7 @@ $(document).ready(function () {
                     break;
             }
             this.index = index;
-            console.log(isPlayed)
+            isPlayed = play ? true : isPlayed;
             this.play(isPlayed);
         }
 
@@ -97,6 +107,19 @@ $(document).ready(function () {
             if(isPlayed) this.stopSong();
             this.index = index;
             this.play(isPlayed);
+        }
+
+        seekTo(secs) {
+            var sound = this.songs[this.index].howl;
+            if(sound) {
+                if(sound.playing()) {
+                    sound.seek(secs);
+                    return;
+                }
+            }
+            this.play();
+            sound = this.songs[this.index].howl
+            sound.seek(secs);
         }
 
     }
@@ -168,5 +191,29 @@ $(document).ready(function () {
         let index = $(this).attr('data-id');
         scrollDown.setEndValue(0);
         player.skipTo(index);
+    })
+
+    $("#progress-bar").change(function(e) {
+        console.log('change')
+        player.seekTo($(this).val());
+    });
+
+    $("#progress-bar").on("mousedown", () => {
+        console.log('down')
+        player.onHover = true;
+    })
+
+    $("#progress-bar").on("mouseup", () => {
+        player.onHover = false;
+        console.log('up')
+        setTimeout(() => {
+            player.seekTo($(this).val());
+            player.step();
+        }, 0);
+    })
+
+    $(".ion-md-repeat").click(function (e) {
+        e.preventDefault();
+        player.setReply();
     })
 });
